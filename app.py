05ee70398d57c08d11a5aafb7bdd032b1e393ba1
Цыганова Дарья <135041193+tsda04@users.noreply.github.com>
2025-11-23 01:49:35 +0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from flask import Flask
+import datetime
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def health():
+    return {'status': 'healthy', 'timestamp': datetime.datetime.now().isoformat()}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8181, debug=False)
